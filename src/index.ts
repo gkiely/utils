729fce70c334c6b62163f3e47f5908346cb4db0f
Grant Kiely <grant.kiely@gmail.com>
2022-11-05 10:38:39 -0500

    shaved off 14 bytes by removing function keyword

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import fastDeepEqual from 'fast-deep-equal';
 
 export const isEqual = fastDeepEqual as <A, B>(a: A, b: B) => boolean;
 
-export function assertType<T>(_: unknown): asserts _ is T {}
+export const assertType = <T>(_: unknown): asserts _ is T => {};
 
-export function pick<T extends object, K extends keyof T>(
+export const pick = <T extends object, K extends keyof T>(
   obj: T,
   ...keys: K[]
-) {
+) => {
   const result: Partial<T> = {};
   for (const key of keys) {
     if (key in obj) {
@@ -15,7 +15,7 @@ export function pick<T extends object, K extends keyof T>(
     }
   }
   return result as Required<Pick<T, K>>;
-}
+};
 
 export const isObject = (value: unknown): value is object => {
   return typeof value === 'object' && !Array.isArray(value) && value !== null;
@@ -36,6 +36,37 @@ export const delay = (ms: number) =>
 
 const print = console.log.bind(console);
 
-export function log(...args: unknown[]) {
+export const log = (...args: unknown[]) => {
   return print(...args);
-}
+};
+
+type Options = {
+  method?: 'GET' | 'POST';
+  headers?: Record<string, string>;
+  body?: Record<string, unknown>;
+};
+
+export const fetchJSON = async <Data = unknown>(
+  url: string,
+  options?: Options
+) => {
+  if (!options || !('body' in options)) {
+    const response = await fetch(
+      url,
+      options ? (options as Omit<Options, 'body'>) : undefined
+    );
+    return response.json<Data>();
+  }
+  const body = JSON.stringify(options.body);
+  const response = await fetch(url, {
+    ...options,
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      accept: 'application/json',
+      ...options.headers,
+    },
+    body,
+  });
+  return response.json<Data>();
+};
